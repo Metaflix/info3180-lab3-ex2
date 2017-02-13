bad1a066faa8557d69a69b0f78e8653ba0b45827
Kurt Edmondson <edmondson.kurt@gmail.com>
2017-02-13 02:12:02 +0000

    final ting i think

--- app/views.py
+++ app/views.py
@@ -7,7 +7,8 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
-
+from app.ContactForm import ContactForm
+import smtplib
 
 ###
 # Routing for your application.
@@ -23,6 +24,33 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        message = """From: {} <{}>
+        To: {} <{}>
+        Subject: {}
+        {}"""
+        fromname = form.Name.data
+        fromaddr = form.Email.data
+        subject = form.Subject.data
+        msg = form.Message.data
+        toname = 'Roxy Reid'
+        toaddr = 'leonardo.a.grant@gmail.com'
+        messagetosend = message.format(
+            fromname, fromaddr, toname, toaddr, subject, msg)
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login('therox116@gmail.com', 'owoodmbxqpdjfihs')
+        server.sendmail(fromaddr, toaddr, messagetosend)
+        server.quit()
+        return render_template('ContactForm.html', form=form)
+    else:
+        return render_template('ContactForm.html', form=form)
+    
+    return render_template('contact.html', name= "Contact Us")
 
 
 ###
@@ -51,7 +79,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port="8080")
